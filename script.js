7b83fa805fd485903f1bbe0a719e571239eafdfd
mpaulweeks <mpaulweeks@gmail.com>
2017-08-13 17:56:13 -0400

    use first-child

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 (function (){
   function getWeek(weekNum){
-    let css = weekNum == 1 ? ' week-first' : '';
     return `
-      <div class="week${css}">
+      <div class="week">
         <div class="week-title">WEEK ${weekNum}</div>
         <div class="contents">
           <a href="https://github.com/mpaulweeks/cfc2017/sample/week${weekNum}">Sample</a>
