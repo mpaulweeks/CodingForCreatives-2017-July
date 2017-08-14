37482c6017ef3f99155eb97b220105748d4d963f
mpaulweeks <mpaulweeks@gmail.com>
2017-08-14 01:23:34 -0400

    make welcome dir, change em -> px

---
+++ welcome/script.js
@@ -0,0 +1,23 @@
+(function (){
+  function getWeek(weekNum){
+    return `
+      <div class="week">
+        <div class="week-title">WEEK ${weekNum}</div>
+        <div class="contents">
+          <a href="https://github.com/mpaulweeks/cfc2017/sample/week${weekNum}">Sample</a>
+        </div>
+      </div>`;
+  }
+  var weekData = '';
+  for (var i = 1; i <= 3; i++){
+    weekData += getWeek(i);
+  }
+  document.getElementById('week-data').innerHTML = weekData;
+
+  let heros = [
+    'kermit',
+    'cat',
+  ];
+  let randomHero = heros[Math.floor(Math.random() * heros.length)];
+  document.getElementById('hero').style.backgroundImage = `url(welcome/${randomHero}.gif)`;
+})()
