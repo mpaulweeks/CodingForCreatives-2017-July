17efa1ede7afca494d2261f455599d7efc840af9
mpaulweeks <mpaulweeks@gmail.com>
2017-08-19 14:42:50 -0400

    add testing tools

---
+++ tools/test/example.js
@@ -0,0 +1,13 @@
+
+window.Tester(function(T){
+  T.assert(
+    'True is true?',
+    true,
+    'Data for true'
+  );
+  T.assert(
+    'False is true?',
+    false,
+    'Data for false'
+  );
+});
---
+++ tools/tester.js
@@ -0,0 +1,47 @@
+window.Tester = function(assertions){
+
+    let failures = 0;
+    function start(){
+        document.body.innerHTML = `
+          <div id="title"></div>
+          <hr/>
+          <div id="results"></div>
+        `;
+        document.body.classList.add('working');
+    }
+    function finish(){
+        const title = document.getElementById('title');
+        document.body.classList.remove('working');
+        if(failures == 0){
+            title.innerHTML = "ALL TESTS PASS";
+            title.classList.add('success');
+        } else {
+            title.innerHTML = `TEST FAILURES: ${failures}`;
+            title.classList.add('failure');
+        }
+    }
+    function assert(message, value, data){
+        const passed = Boolean(value);
+        const prettyPassed = passed ? "PASSED" : "FAILED";
+        let debug = "";
+        if (!passed){
+            failures += 1;
+            if (data){
+                console.log(message, data);
+                debug = `<pre class="debug">${data}</pre>`;
+            }
+        }
+        document.getElementById('results').innerHTML += `
+            <div class="test ${passed}">
+                <b>${prettyPassed}</b> ${message}
+                ${debug}
+            </div>
+        `;
+    }
+
+    start();
+    assertions({
+        assert: assert
+    });
+    finish();
+};
