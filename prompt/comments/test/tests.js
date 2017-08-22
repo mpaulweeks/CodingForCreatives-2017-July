484b4b5e5f4feddac04ac367a45cb456eef9229f
mpaulweeks <mpaulweeks@gmail.com>
2017-08-22 16:51:47 -0400

    fix comments test

--- prompt/comments/test/tests.js
+++ prompt/comments/test/tests.js
@@ -3,15 +3,15 @@ CFC.Comments.FilterAndDisplay = function(){}
 
 CFC.Tester(function (T){
   var exists = T.assert(
-    'filterComments is defined',
-    typeof filterComments !== 'undefined',
+    'filterAdminComments is defined',
+    typeof filterAdminComments !== 'undefined',
   );
   if (!exists){
     return
   }
   T.assertCollectionEqual(
-    'filterComments on empty returns empty',
+    'filterAdminComments on empty returns empty',
     [],
-    filterComments([]),
+    filterAdminComments([]),
   );
 });
