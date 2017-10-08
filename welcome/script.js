e7a6a26408259db479f841a326bb3b5e031afc24
mpaulweeks <mpaulweeks@gmail.com>
2017-10-07 23:19:14 -0400

    include links to projects on homepage

--- welcome/script.js
+++ welcome/script.js
@@ -5,6 +5,23 @@
 
   function renderLinks(roster){
     console.log(roster);
+    var bodyHtml = '';
+    roster.students.forEach(s => {
+      var sHtml = `<td class="projects-name">${s}</td>`;
+      roster.projects.forEach(p => {
+        sHtml += `<td class="projects-cell"><a target="_blank" href="https://github.com/mpaulweeks/cfc2017/tree/master/students/${s}/${p}">${p}</a></td>`;
+      });
+      bodyHtml += `<tr>${sHtml}</tr>`;
+    });
+    document.body.innerHTML += `
+      <div class="projects-container">
+        <table>
+          <tbody>
+            ${bodyHtml}
+          </tbody>
+        </table>
+      </div>
+    `;
   }
   fetch("http://static.mpaulweeks.com/cfc2017/roster.json")
     .then(response => {
