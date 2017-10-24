d9540c28cd7560b6889b4cff5d32463dbfe482a2
mpaulweeks <mpaulweeks@gmail.com>
2017-10-23 21:00:30 -0400

    drawTriangle does all edges

--- static/canvas.js
+++ static/canvas.js
@@ -36,6 +36,7 @@ CFC.NewCanvas = function(canvasId){
       ctx.moveTo(x1, y1);
       ctx.lineTo(x2, y2);
       ctx.lineTo(x3, y3);
+      ctx.lineTo(x1, y1);
       ctx.stroke();
     }
     var fillTriangle = function(colorStr, x1, y1, x2, y2, x3, y3){
