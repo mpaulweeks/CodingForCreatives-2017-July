87fc39a96fd6655f58dd1f168174b9f5c54667f8
mpaulweeks <mpaulweeks@gmail.com>
2017-10-24 20:57:27 -0400

    in class changes

--- homework/canvas/example/script.js
+++ homework/canvas/example/script.js
@@ -1,3 +1,24 @@
 // starting a line in JS with two slashes makes it a comment
 // comments are ignored by the browser
 // they are useful for documenting your code
+
+var stupid = CFC.NewCanvas("artwork");
+
+var gridColor = "blue";
+var gridWidth = 100;
+
+var originX = 500;
+var originY = 600;
+
+
+stupid.drawLine(gridColor, gridWidth*1, 0, gridWidth*1, 700);
+stupid.drawLine(gridColor, gridWidth*2, 0, gridWidth*2, 700;
+
+var greenStuff = "green";
+stupid.drawLine(greenStuff, gridWidth*3, 0, gridWidth*3, 700);
+stupid.drawLine(greenStuff, gridWidth*4, 0, gridWidth*4, 700);
+stupid.drawLine(greenStuff, gridWidth*5, 0, gridWidth*5, 700);
+
+stupid.drawLine(gridColor, 0, 100, 1000, 100);
+
+stupid.drawTriangle("black", 150, 150, 250, 150, 250, 250);
