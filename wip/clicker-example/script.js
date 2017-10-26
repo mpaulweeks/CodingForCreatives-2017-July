eec55dbbd09d864e01f7f7990202881391bf5dc2
mpaulweeks <mpaulweeks@gmail.com>
2017-10-26 10:42:59 -0400

    simplify clicker for class example

---
+++ wip/clicker-example/script.js
@@ -0,0 +1,35 @@
+
+var cookieCount = 0;
+var grandmaCount = 0;
+
+var divCookieCounter = document.getElementById("cookie-counter");
+
+var buttonBakeCookie = document.getElementById("bake");
+var buttonBuyGrandma = document.getElementById("buy-grandma");
+
+var refreshDisplays = function(){
+  divCookieCounter.innerHTML = cookieCount;
+}
+var bakeCookie = function(){
+  cookieCount = cookieCount + 1;
+  refreshDisplays();
+}
+var buyGrandma = function(){
+  var grandmaPrice = 10;
+  if (cookieCount >= grandmaPrice){
+    grandmaCount = grandmaCount + 1;
+    cookieCount = cookieCount - grandmaPrice;
+    grandmaPrice = grandmaPrice + grandmaCount;
+    refreshDisplays();
+  }
+}
+var loop = function(){
+  cookieCount = cookieCount + (grandmaCount * 1);
+  refreshDisplays();
+}
+
+buttonBakeCookie.addEventListener('click', bakeCookie);
+buttonBuyGrandma.addEventListener('click', buyGrandma);
+setInterval(loop, 1000);
+
+refreshDisplays();
