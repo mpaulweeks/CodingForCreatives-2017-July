2f333fd44f909d1a00c9dbd07c00ed0424a68972
jorjorb3 <32470446+jorjorb3@users.noreply.github.com>
2017-11-05 16:50:30 -0500

    Add files via upload

--- students/jordan/clicker/script.js
+++ students/jordan/clicker/script.js
@@ -8,61 +8,63 @@ var factoryCost = 25;
 var totalCookieCount = 0;
 
 var spanCookieCount = document.getElementById("cookie-count");
-var spanGrandmaCount = document.getElementById("grandma-count");
-var spanFactoryCount = document.getElementById("factory-count");
 var spanTotalCookieCount = document.getElementById("total-cookie-count");
 var buttonBakeCookie = document.getElementById("bake-cookie");
 var buttonEnlistGrandma = document.getElementById("enlist-grandma");
-var spanGrandmaCost = document.getElementById("grandma-cost")
-var spanFactoryCost = document.getElementById("factory-cost")
+var spanGrandmaCount = document.getElementById("grandma-count");
+var spanGrandmaCost = document.getElementById("grandma-cost");
+var spanFactoryCount = document.getElementById("factory-count");
+var spanFactoryCost = document.getElementById("factory-cost");
 var buttonBuyFactory = document.getElementById("buy-factory");
+
 var divMessage = document.getElementById("message");
 
 var funcUpdateHTML = function(){
-  if (cookieCount > 5){
-    divMessage.innerHTML = "You are one smart cookie!"
-  }
-  if (cookieCount > 20){
-    divMessage.innerHTML = "I smell success in your future!"
-  }
-  if (cookieCount > 45){
-    divMessage.innerHTML = "Sweeeeeet, bro!!!!"
-  }
-  if (cookieCount > 99){
-    divMessage.innerHTML = "You got 99 problems, but bakin' ain't one!"
-  }
   spanCookieCount.innerHTML = cookieCount;
   spanGrandmaCount.innerHTML = grandmaCount;
-  spanFactoryCount.innerHTML = factoryCount;
-  spanTotalCookieCount.innerHTML = totalCookieCount;
   spanGrandmaCost.innerHTML = grandmacost;
+  spanFactoryCount.innerHTML = factoryCount;
   spanFactoryCost.innerHTML = factorycost;
+  spanTotalCookieCount.innerHTML = totalCookieCount;
+if (cookieCount > 5){
+  divMessage.innerHTML = "You are one smart cookie!"
+}
+if (cookieCount > 20){
+  divMessage.innerHTML = "I smell success in your future!"
+}
+if (cookieCount > 45){
+  divMessage.innerHTML = "Sweeeeeet, bro!!!!"
+}
+if (cookieCount > 99){
+  divMessage.innerHTML = "You got 99 problems, but bakin' ain't one!"
+}
 }
 var funcBakeCookie = function(){
   cookieCount = cookieCount + 1;
+  totalCookieCount = totalCookieCount + 1;
   funcUpdateHTML();
 }
 var funcEnlistGrandma = function(){
-  if ( cookieCount >= grandmaCost ) {
+  if (cookieCount >= grandmaCost) {
     cookieCount = cookieCount - grandmaCost;
     grandmaCount = grandmaCount + 1;
-    grandmaCost = grandmaCost + 5;
-    funcUpdateHTML();
+    grandmaCost = grandmaCost * 2;
   }
 }
 
 var funcBuyFactory = function(){
-  if ( cookieCount >= factoryCost ) {
+  if (cookieCount >= factoryCost) {
     cookieCount = cookieCount - factoryCost;
     factoryCount = factoryCount + 1;
-    factoryCost = factoryCost + 10;
     funcUpdateHTML();
-    }
+    factoryCost = factoryCost * 3;
+  }
 }
-
 var funcLoop = function(){
-  cookieCount = cookieCount + grandmaCount + (factoryCount*5);
-  totalCookieCount = totalCookieCount + grandmaCount + (factoryCount*5)
+  cookieCount = cookieCount + grandmaCount;
+  totalCookieCount = totalCookieCount + grandmaCount;
+  cookieCount = cookieCount + (factoryCount * 100);
+  totalCookieCount = totalCookieCount + (factoryCount * 100);
   funcUpdateHTML();
 }
 
