586bdda388733e93a8399669a8d7ffd354fe1e08
mpaulweeks <mpaulweeks@gmail.com>
2017-11-19 21:02:42 -0500

    use test data for example

--- lectures/week8/comments/script.js
+++ lectures/week8/comments/script.js
@@ -1,2 +1,2 @@
-CFC.Comments.Setup("christo", "view-christo", "form-christo");
-CFC.Comments.Setup("marina", "view-marina", "form-marina");
+CFC.Comments.Setup("test-1", "view-test-1", "form-test-1");
+CFC.Comments.Setup("test-2", "view-test-2", "form-test-2");
